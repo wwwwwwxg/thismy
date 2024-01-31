12db8b9063d6d277538c8c945fe914441364e681
root <root@root.hgzjpm5ntvfevfbh2psnn15ivh.hx.internal.cloudapp.net>
2024-01-31 23:47:43 +0000

    Add helper_58.py: 1/31/2024

---
+++ src/helper/helper_58.py
@@ -0,0 +1,7 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
