c8d2837d697eb3618d5ffd280fce9f50aad5e4db
wwwwwwxg <wwwwwwxg@users.noreply.github.com>
2025-01-23 13:28:58 +0000

    Add service_881.js: 1/23/2025

---
+++ src/component/component_752.js
@@ -0,0 +1,7 @@
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+  const pivot = arr[0];
+  const left = arr.slice(1).filter(x => x < pivot);
+  const right = arr.slice(1).filter(x => x >= pivot);
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
